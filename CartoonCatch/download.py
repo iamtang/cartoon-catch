17e3df2a842c7c035c4a5d95799f3884ba1f00b6
jj <tangjun@zuzuche.com>
2022-02-25 16:04:41 +0800

    fix bug

--- CartoonCatch/download.py
+++ CartoonCatch/download.py
@@ -5,10 +5,12 @@ import threading
 
 lock = threading.Lock()
 
-def url_response(url):
+def url_response(url,tq):
     myfile = requests.get(url['src'], headers={"user-agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.1.3 Mobile/15E148 Safari/604.1"})
     creatDir(url['path'])
     open(url['path'] + url['fileName'], 'wb').write(myfile.content)
+    tq.update(1)
+
 
 def download(urls, options, title):
     threads_num = options.get('threads_num', 5)
@@ -19,9 +21,8 @@ def download(urls, options, title):
         try:
             for i in range(0, threads_num):
                 img = next(it)
-                t = threading.Thread(target=url_response, args=(img,))
+                t = threading.Thread(target=url_response, args=(img,tq))
                 t.start()
-                tq.update(1)
         except StopIteration:
             break
         
